078e7653371701fbd28111cd0269e47eb73d3968
Nakamura <anmituseries@yahoo.co.jp>
2019-06-11 21:00:28 +0900

    [add] PWM LL sample

---
+++ STM32F303_PWM_LL/Src/PWM.hpp
@@ -0,0 +1,26 @@
+/*
+ * PWM.hpp
+ *
+ *  Created on: Jun 9, 2019
+ *      Author: watashi
+ */
+
+#include "stm32f3xx_ll_tim.h"//need
+
+
+#ifndef PWM_HPP_
+#define PWM_HPP_
+
+class PWM {
+private:
+	TIM_TypeDef *mTIMx;
+	int mCH=0;
+	__IO uint32_t *mREG;
+public:
+	void setTIM(TIM_TypeDef* pTIMx);
+	void setCH(int pCH);
+	void Init();
+	void Duty(int pDuty);
+};
+
+#endif /* PWM_HPP_ */
