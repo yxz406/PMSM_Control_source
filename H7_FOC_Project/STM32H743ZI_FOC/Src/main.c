82b04f0e8a2ab87eb83b0e25c22530d6616ada3c
yutaka.N <ioloa.yutaka@gmail.com>
2019-12-10 23:24:01 +0900

    [add] MAXONで矯正転流

--- H7_FOC_Project/STM32H743ZI_FOC/Src/main.c
+++ H7_FOC_Project/STM32H743ZI_FOC/Src/main.c
@@ -181,7 +181,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 1;
-  RCC_OscInitStruct.PLL.PLLN = 100;
+  RCC_OscInitStruct.PLL.PLLN = 120;
   RCC_OscInitStruct.PLL.PLLP = 2;
   RCC_OscInitStruct.PLL.PLLQ = 4;
   RCC_OscInitStruct.PLL.PLLR = 2;
